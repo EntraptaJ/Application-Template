c9088c840ea650824f9a40e5a723e3a6d0543b64
Kristian F Jones <me@kristianjones.xyz>
2019-10-04 13:21:42 -0500

    fix: recursive useRoute

--- Web/UI/Components/Router/useRoute.ts
+++ Web/UI/Components/Router/useRoute.ts
@@ -1,10 +1,22 @@
 // Web/UI/Components/Router/useRoute.ts
 import { useLocation } from 'react-router';
 import { Routes, Route } from './Routes';
-import { useMemo } from 'react';
+import { useCallback } from 'react';
 
 export function useRoute(): Route | undefined {
   const { pathname } = useLocation();
 
-  return useMemo(() => Routes.find(({ to }) => to === pathname), [pathname]);
+  const findRoute = useCallback(
+    (routes: Route[]): Route | undefined => {
+      for (const route of routes) {
+        if (route.to === pathname || route.to === pathname.replace(/\/$/, ''))
+          return route;
+        else if (route.children && pathname.includes(route.to))
+          return findRoute(route.children);
+      }
+    },
+    [pathname],
+  );
+
+  return findRoute(Routes);
 }
