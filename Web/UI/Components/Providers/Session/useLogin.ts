1525ca62f80d8c7a4076fb512192e6d6216e5e7f
Kristian Jones <me@kristianjones.xyz>
2019-10-05 12:24:29 -0500

    fix: Fix Child routes loading all direct parents components (#4)

--- Web/UI/Components/Providers/Session/useLogin.ts
+++ Web/UI/Components/Providers/Session/useLogin.ts
@@ -8,7 +8,9 @@ type LoginFunction = (input: LoginInput) => Promise<boolean>;
 
 export function useLogin(): [LoginFunction, LoginMutationResult] {
   const { setToken } = useToken();
-  const [loginFn, extras] = useLoginMutation();
+  const [loginFn, extras] = useLoginMutation({
+    context: { ignoreError: true },
+  });
   const login: LoginFunction = useCallback(
     async (input) => {
       const response = await loginFn({ variables: { input } });
